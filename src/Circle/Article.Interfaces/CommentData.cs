6ee9dd8f3ea3e7c571d7869dff6a6ea3e4dca70d
hoshiyasu <h_hoshiyasu@jbcc.co.jp>
2016-12-26 16:48:41 +0900

    CommentList

--- src/Circle/Article.Interfaces/CommentData.cs
+++ src/Circle/Article.Interfaces/CommentData.cs
@@ -11,5 +11,39 @@ namespace Article.Interfaces
     /// </summary>
     public class CommentData
     {
+        /// <summary>
+        /// フィード管理ID
+        /// </summary>
+        public string FeedID { get; set; }
+
+        /// <summary>
+        /// コメント管理ID
+        /// </summary>
+        public string CommentID { get; set; }
+
+        /// <summary>
+        /// コメント
+        /// </summary>
+        public string CommentBody { get; set; }
+
+        /// <summary>
+        /// コメント投稿者ID
+        /// </summary>
+        public string CommentUserID { get; set; }
+
+        /// <summary>
+        /// コメント投稿日時
+        /// </summary>
+        public DateTime CommPostDateTime { get; set; }
+
+        /// <summary>
+        /// コメント更新者ID
+        /// </summary>
+        public string CommUpdateUserID { get; set; }
+
+        /// <summary>
+        /// コメント更新日時
+        /// </summary>
+        public DateTime CommUpdateDateTime { get; set; }
     }
 }
